b821e62ed439afe34cae05ec5ac0f79b2726b173
paula dobre <paula.dobre24@gmail.com>
2023-11-03 17:37:56 +0200

    destinations

--- blog-app/src/app/components/destinations/destinations.component.ts
+++ blog-app/src/app/components/destinations/destinations.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Post } from 'src/app/models/featured-post';
 import { PostService } from 'src/app/services/post.service';
+import { ElementRef, Renderer2 } from '@angular/core';
+
 
 @Component({
   selector: 'app-destinations',
@@ -12,15 +14,21 @@ export class DestinationsComponent {
   selectedDestination: string | null = null;
   articles: Post[] = [];
 
-  constructor(private postService: PostService) { }
+  constructor(private postService: PostService, private el: ElementRef, private renderer: Renderer2) { }
 
   loadArticles(destination: string) {
     console.log('Loading articles for:', destination);
     this.selectedDestination = destination;
     this.postService.loadPostsByDestination(destination).subscribe(articles => {
-      console.log('Articles received:', articles);
-      this.articles = articles;
+        console.log('Articles received:', articles);
+        this.articles = articles;
+
+      
+        const articlesSection = this.el.nativeElement.querySelector('#articles-section');
+      if (articlesSection) {
+        articlesSection.scrollIntoView({ behavior: 'smooth', block: 'end' });
+      }
     });
-  }
+}
   
 }
